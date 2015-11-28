05a1519b1c009476372ee36d78961c0728162342
chansuke <moonset20@gmail.com>
2015-11-29 01:16:58 +0900

    Add ruby sample scipt

--- scripts/ruby/darui.rb
+++ scripts/ruby/darui.rb
@@ -12,4 +12,20 @@ TWILIO_AUTH_TOKEN  = ENV['TWILIO_AUTH_TOKEN']
 
 @twilio = Twilio::REST::Client.new TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN
 
+my_number   = 'xxx'
+boss_number = 'yyy'
 
+excuse = [
+  '体調が芳しくないので',
+  '頭痛が酷いので',
+  '家の用事があるので'
+].sample
+
+# Send a text message
+@twilio.messages.create(
+  from: my_number, to: number_of_boss,
+  body: "#{excuse},自宅稼働します。"
+)
+
+# Log this
+puts "送信時刻: #{Time.now} | Excuse: #{excuse}"
