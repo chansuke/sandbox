1442b6ab73a176619d7927441df76590b2b82bb5
chansuke <moonset20@gmail.com>
2015-12-06 01:19:53 +0900

    some change

--- C/lispy/prompt.c
+++ C/lispy/prompt.c
@@ -1,18 +1,40 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-static char input[2048];
+#ifdef _WIN32
+#include <string.h>
+
+static char buffer[2048];
+
+char* readline(char* prompt) {
+  fputs(prompt, stdout);
+  fgets(buffer, 2048, stdin);
+  char* cpy = malloc(strlen(buffer)+1);
+  strcpy(cpy, buffer);
+  cpy[strlen(cpy)-1] = '\0';
+  return cpy;
+}
+
+void add_history(char* unused) {}
+
+#else
+#include <editline/readline.h>
+#endif
 
 int main(int argc, char** argv) {
-	puts("Lispy Version 0.1");
+
+	puts("Lisper Version 0.1");
 	puts("Press Ctrl+c to Exit\n");
-	
+
 	while (1) {
-		fputs("lispy> ", stdout);
-		
-		fgets(input, 2048, stdin);
-		
+
+    char*input=readline("lisper> ");
+    add_history(input);
+
 		printf("No you're a %s", input);
+    free(input);
+
 	}
-	
+
 	return 0;
-}		
+}
